4183e776a28452d9769f3f86d07003bd7e4034d0
Kumar Saurabh <55243385+KS9004@users.noreply.github.com>
2020-07-02 15:02:31 +0530

    added carousel images plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:carousel_pro/carousel_pro.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
@@ -14,6 +14,22 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    Widget image_carousel = Container(
+      height: 200.0,
+      child: Carousel(
+        boxFit: BoxFit.cover,
+        images: [
+          AssetImage('images/c1.jpg'),
+          AssetImage('images/m1.jpeg'),
+          AssetImage('images/w1.jpeg')
+        ],
+        autoplay: true,
+        dotSize: 4.0,
+        indicatorBgPadding: 2.0,
+        animationCurve: Curves.fastOutSlowIn,
+        animationDuration: Duration(milliseconds: 1000),
+      ),
+    );
     return Scaffold(
       appBar: AppBar(
         elevation: 0.1,
@@ -50,6 +66,22 @@ class _HomePageState extends State<HomePage> {
 
           ],
         ),
+      ),
+      body: ListView(
+        children: [
+//          Carousel Images
+          image_carousel,
+          Padding(padding: EdgeInsets.all(10.0),
+          child: Text("Categories",style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold
+          ),
+          ),
+          ),
+//          Horizontal ListView
+
+
+        ],
       ),
     );
   }
